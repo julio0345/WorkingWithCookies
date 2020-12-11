1cc3fb78a28701f76a8f0f32db7d258e8bf28d50
Júlio César <julio0345@gmail.com>
2020-12-10 23:33:01 -0300

    Just Trainning with Cookies

---
+++ WorkingWithCookies/WorkingWithCookies/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkingWithCookies.Models;
+
+namespace WorkingWithCookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       public IActionResult CreateCookies()
+        {
+            string key = "language";
+            string value = "pt-br";
+            CookieOptions cookieOptions = new CookieOptions();
+
+            cookieOptions.Expires = DateTime.Now.AddDays(365);
+            Response.Cookies.Append(key, value, cookieOptions);
+            return View("Index");
+        }
+
+        public IActionResult ReadCookies()
+        {
+            string key = "language";
+
+            var cookieLanguage = Request.Cookies[key];
+            
+            if(cookieLanguage != null && cookieLanguage.ToString().Equals("pt-br1"))
+            {
+                return View("Index") ;
+            }
+            return View("Privacy");
+        }
+
+        public IActionResult RemoveCookies()
+        {
+            string key = "language";
+            string value = "";
+            CookieOptions cookieOptions = new CookieOptions();
+
+            cookieOptions.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Append(key, value, cookieOptions);
+            return View("Index");
+        }
+    }
+}
